54190993cefea1cc504e0c003ccf45bb95780ede
BhavikEncircle <bhavikp.encircle@gmail.com>
2026-01-28 19:08:58 +0530

    Initated Task 2

---
+++ Task 2/server.js
@@ -0,0 +1,28 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+const server = http.createServer((req, res) => {
+  let filepath = "";
+
+  if (req.url === "/") {
+    filepath = path.join(__dirname, "public", "index.html");
+  } else if (req.url === "/about") {
+    filepath = path.join(__dirname, "public", "about.html");
+  } else {
+    filepath = path.join(__dirname, "public", "index.html");
+  }
+
+  fs.readFile(filepath, (err, content) => {
+    if (err) {
+      res.writeHead(404);
+      res.end("File not found");
+    } else {
+      res.writeHead(200);
+      res.end(content);
+    }
+  });
+});
+
+server.listen(3000, () => {
+  console.log("server is running on port 3000.");
+});
